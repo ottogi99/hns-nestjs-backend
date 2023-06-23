18e517f1f9b842490065b3c09e19fdf4bdc65b31
seonghwa <ottogi99@gmail.com>
2023-06-23 12:00:14 +0900

    applying validationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 // import * as dotenv from 'dotenv';
 // import * as path from 'path';
 
@@ -12,6 +13,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+  }));
   await app.listen(3000);
 }
 bootstrap();
--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,6 +1,16 @@
+import { IsEmail, IsString, MaxLength, MinLength } from "class-validator";
+
 export class CreateUserDto {
+  @IsString()
+  @IsEmail()
+  @MaxLength(60)
   readonly email: string;
+
   readonly password: string;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(30)
   readonly username: string;
 }
 
